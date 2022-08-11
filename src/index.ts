96bbaeea1d3c66130c5eac1ca7b41dee6d14394a
necm1 <necmi.m.hasan@outlook.de>
2022-08-11 11:20:59 +0200

    [UPDATE] index.ts - added temporary instance init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export { Encrypter } from './encrypter'
+import {Encrypter} from './encrypter';
+import {Algorithm} from './algorithm.enum';
+
+new Encrypter('base64:u2D3+em3E9/O3hSUHJAKTg==', Algorithm.AES128gcm).generateKey();
+
+export {Encrypter} from './encrypter'
