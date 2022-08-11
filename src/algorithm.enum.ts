5271c22d062679ae19348131eaa6dbf7a49983f5
necm1 <necmi.m.hasan@outlook.de>
2022-08-11 11:20:06 +0200

    [CREATE] algorithm.enum.ts

---
+++ src/algorithm.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * @enum Algorithm
+ */
+export enum Algorithm {
+  'AES128cbc' = 'AES-128-cbc',
+  'AES256cbc' = 'AES-256-cbc',
+  'AES128gcm' = 'AES-128-gcm',
+  'AES256gcm' = 'AES-256-gcm'
+}
